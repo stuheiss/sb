bd56f903e399189a6d8f56b3eb6eb306bdf36935
Stu Heiss <sheiss@highlandsolutions.com>
2020-10-25 12:15:45 -0500

    Change welcome msg

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Demo Vue App Deploy to GitHub"/>
   </div>
 </template>
 
